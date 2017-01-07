439dd23e09d6ed5fb335a4c029bd6baeffe1a8ab
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-01-07 06:21:35 -0800

    Add Heroku production config

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,24 @@ use Mix.Config
 # which you typically run after static files are built.
 config :people_pets_party, PeoplePetsParty.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [
+    scheme: "https",
+    host: "https://ancient-dawn-31926.herokuapp.com",
+    port: 443
+  ],
+  force_ssl: [ rewrite_on: [ :x_forwarded_proto ] ],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Database:
+config :people_pets_party, PeoplePetsParty.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
@@ -58,4 +71,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
